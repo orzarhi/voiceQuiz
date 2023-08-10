35af89c96ce1a0f852b479cc01e0a6823e3da064
Maxsoft Ltd <95671078+MaxsoftLtd@users.noreply.github.com>
2023-08-10 19:33:55 +0300

    New questions

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,102 +1,182 @@
 export const Questions = [
     {
-        questionText: "Serendipity",
-        answerOptions: [
-            { answerText: "המצאה מקרית", isCorrect: true },
-            { answerText: "תעלומה", isCorrect: false },
-            { answerText: "שוקט", isCorrect: false },
-            { answerText: "הקולות השונים המשתוללים יחד", isCorrect: false },
-        ],
-    },
-    {
-        questionText: "Mellifluous",
-        answerOptions: [
-            { answerText: "נמלט", isCorrect: false },
-            { answerText: "כדי להגן על המיטב בשלמות נפש", isCorrect: false },
-            { answerText: "מתוק ונעים", isCorrect: true },
-            { answerText: "קר דם", isCorrect: false },
-        ],
-    },
-    {
-        questionText: "Obfuscate",
-        answerOptions: [
-            { answerText: "להעמיק או להערב דברים", isCorrect: true },
-            { answerText: "מתעמיק בדבר, מחושב, מתבונן", isCorrect: false },
-            { answerText: "להסתיר", isCorrect: false },
-            { answerText: "חלום, פנטזיה", isCorrect: false },
-        ],
-    },
-    {
-        questionText: "Quixotic",
-        answerOptions: [
-            { answerText: "תעלומה", isCorrect: false },
-            { answerText: "חלום, פנטזיה", isCorrect: false },
-            { answerText: "רע, רקיע, מזיק, חבלני", isCorrect: false },
-            { answerText: "עקבי לנקוט פעולה מדהימה או בלתי מתונה", isCorrect: true },
-        ],
-    },
-    {
-        questionText: "Enigma",
-        answerOptions: [
-            { answerText: "מתעמיק בדבר, מחושב, מתבונן", isCorrect: false },
-            { answerText: "הקולות השונים המשתוללים יחד", isCorrect: false },
-            { answerText: "תעלומה", isCorrect: true },
-            { answerText: "כדי להגן על המיטב בשלמות נפש", isCorrect: false },
-        ],
-    },
-    {
-        questionText: "Cacophony",
-        answerOptions: [
-            { answerText: "המצאה מקרית", isCorrect: false },
-            { answerText: "תעלומה", isCorrect: false },
-            { answerText: "הקולות השונים המשתוללים יחד", isCorrect: true },
-            { answerText: "שוקט", isCorrect: false },
-        ],
-    },
-    {
         questionText: "Ephemeral",
         answerOptions: [
-            { answerText: "נמלט", isCorrect: false },
-            { answerText: "קר דם", isCorrect: false },
-            { answerText: "שוקט", isCorrect: false },
-            { answerText: "נמלט", isCorrect: true },
-        ],
-    },
-    {
-        questionText: "Ubiquitous",
-        answerOptions: [
-            { answerText: "חלום, פנטזיה", isCorrect: false },
-            { answerText: "מתעמיק בדבר, מחושב, מתבונן", isCorrect: false },
-            { answerText: "רע, רקיע, מזיק, חבלני", isCorrect: false },
-            { answerText: "מתמצא בכל מקום ובכל זמן", isCorrect: true },
-        ],
-    },
-    {
-        questionText: "Perspicacious",
-        answerOptions: [
-            { answerText: "קר דם", isCorrect: false },
-            { answerText: "מתעמיק בדבר, מחושב, מתבונן", isCorrect: true },
-            { answerText: "חלום, פנטזיה", isCorrect: false },
-            { answerText: "הקולות השונים המשתוללים יחד", isCorrect: false },
+            { answerText: "נצחי", isCorrect: false },
+            { answerText: "זמני", isCorrect: true },
+            { answerText: "רגיל", isCorrect: false },
+            { answerText: "מרשים", isCorrect: false },
         ],
     },
     {
         questionText: "Pernicious",
         answerOptions: [
-            { answerText: "המצאה מקרית", isCorrect: false },
-            { answerText: "חלום, פנטזיה", isCorrect: false },
-            { answerText: "מתמצא בכל מקום ובכל זמן", isCorrect: false },
-            { answerText: "רע, רקיע, מזיק, חבלני", isCorrect: true },
+            { answerText: "מסוכן", isCorrect: true },
+            { answerText: "נעים", isCorrect: false },
+            { answerText: "כואב", isCorrect: false },
+            { answerText: "חכם", isCorrect: false },
         ],
     },
     {
-        questionText: "Altruistic",
+        questionText: "Cacophony",
         answerOptions: [
-            { answerText: "חלום, פנטזיה", isCorrect: false },
-            { answerText: "רע, רקיע, מזיק, חבלני", isCorrect: false },
-            { answerText: "כדי להגן על המיטב בשלמות נפש", isCorrect: true },
-            { answerText: "המצאה מקרית", isCorrect: false },
+            { answerText: "תקשורת", isCorrect: false },
+            { answerText: "שקט", isCorrect: false },
+            { answerText: "הצלילים המשתלשלים יחד", isCorrect: true },
+            { answerText: "רקע", isCorrect: false },
         ],
     },
-
+    {
+        questionText: "Abundant",
+        answerOptions: [
+            { answerText: "פחות", isCorrect: false },
+            { answerText: "מועט", isCorrect: false },
+            { answerText: "רב", isCorrect: true },
+            { answerText: "חד", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Benevolent",
+        answerOptions: [
+            { answerText: "אכזבני", isCorrect: false },
+            { answerText: "נוראי", isCorrect: false },
+            { answerText: "חסיד", isCorrect: true },
+            { answerText: "קר", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Confluence",
+        answerOptions: [
+            { answerText: "יחידה", isCorrect: false },
+            { answerText: "התנגשות", isCorrect: true },
+            { answerText: "שלווה", isCorrect: false },
+            { answerText: "חום", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Debilitate",
+        answerOptions: [
+            { answerText: "חזק", isCorrect: false },
+            { answerText: "חזיק", isCorrect: false },
+            { answerText: "מרותק", isCorrect: false },
+            { answerText: "חולש", isCorrect: true },
+        ],
+    },
+    {
+        questionText: "Ubiquitous",
+        answerOptions: [
+            { answerText: "נפוצה", isCorrect: true },
+            { answerText: "מסתורית", isCorrect: false },
+            { answerText: "חדשה", isCorrect: false },
+            { answerText: "נדירה", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Serene",
+        answerOptions: [
+            { answerText: "רועשת", isCorrect: false },
+            { answerText: "שקטה", isCorrect: true },
+            { answerText: "כועסת", isCorrect: false },
+            { answerText: "נורמלית", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Voracious",
+        answerOptions: [
+            { answerText: "רעבה", isCorrect: true },
+            { answerText: "מזיקה", isCorrect: false },
+            { answerText: "משוגעת", isCorrect: false },
+            { answerText: "פסיכית", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Inevitable",
+        answerOptions: [
+            { answerText: "נמנע", isCorrect: false },
+            { answerText: "לא ידוע", isCorrect: false },
+            { answerText: "לא הגיוני", isCorrect: false },
+            { answerText: "לא נמנע", isCorrect: true },
+        ],
+    },
+    {
+        questionText: "Eloquent",
+        answerOptions: [
+            { answerText: "חסרת ביטוי", isCorrect: false },
+            { answerText: "פתאומית", isCorrect: false },
+            { answerText: "נמניעה", isCorrect: false },
+            { answerText: "נגידה", isCorrect: true },
+        ],
+    },
+    {
+        questionText: "Mundane",
+        answerOptions: [
+            { answerText: "רוחנית", isCorrect: false },
+            { answerText: "פרועה", isCorrect: false },
+            { answerText: "שגרתית", isCorrect: true },
+            { answerText: "מעוררת השראה", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Resilient",
+        answerOptions: [
+            { answerText: "קריצה", isCorrect: false },
+            { answerText: "בריאה", isCorrect: false },
+            { answerText: "עמידה", isCorrect: true },
+            { answerText: "נפלאה", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Eminent",
+        answerOptions: [
+            { answerText: "רגילה", isCorrect: false },
+            { answerText: "גבוהה", isCorrect: true },
+            { answerText: "שונה", isCorrect: false },
+            { answerText: "כחולה", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Elusive",
+        answerOptions: [
+            { answerText: "נכון", isCorrect: false },
+            { answerText: "מוחזק", isCorrect: false },
+            { answerText: "חסר", isCorrect: false },
+            { answerText: "נמלט", isCorrect: true },
+        ],
+    },
+    {
+        questionText: "Vivacious",
+        answerOptions: [
+            { answerText: "כואבת", isCorrect: false },
+            { answerText: "פחותה", isCorrect: false },
+            { answerText: "חסרת חיים", isCorrect: false },
+            { answerText: "שחורה", isCorrect: true },
+        ],
+    },
+    {
+        questionText: "Precarious",
+        answerOptions: [
+            { answerText: "נכון", isCorrect: false },
+            { answerText: "שונה", isCorrect: false },
+            { answerText: "מסוכן", isCorrect: true },
+            { answerText: "עשירה", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Surreptitious",
+        answerOptions: [
+            { answerText: "חשאית", isCorrect: true },
+            { answerText: "גלויה", isCorrect: false },
+            { answerText: "שולית", isCorrect: false },
+            { answerText: "רחבה", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Austere",
+        answerOptions: [
+            { answerText: "שפויה", isCorrect: false },
+            { answerText: "רך", isCorrect: false },
+            { answerText: "פרועה", isCorrect: false },
+            { answerText: "צנועה", isCorrect: true },
+        ],
+    },
 ];
