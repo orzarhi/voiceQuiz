dec152c7ffef4afd6f741568989a3d7235be5c5b
Maxsoft Ltd <95671078+MaxsoftLtd@users.noreply.github.com>
2023-09-11 09:19:56 +0300

    Fixed bug

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,18 +1,29 @@
 import { Header } from '@/components/Header';
 import { Users } from "@/components/Users";
+import { domainConfig } from '@/config/domain';
 import { getAuthSession } from '@/lib/auth';
 import { redirect } from 'next/navigation';
+
+
+export const dataUsers = async () => {
+    const res = await fetch(`${domainConfig.url}/api/users`, { cache: 'no-store' })
+    const data = await res.json()
+
+    return data;
+}
 
 export default async function page({ }) {
     const session = await getAuthSession();
 
     if (!session?.user?.isAdmin) redirect('/')
 
+    const users = await dataUsers();
+
     return (
         <>
             {/* @ts-ignore */}
             <Header session={session} />
-            <Users />
+            <Users users={users} />
         </>
     )
 
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -11,63 +11,61 @@ import { Loading } from '@/components/Loading';
 import { motion } from "framer-motion";
 
 interface UsersProps {
+    users: UserType[]
 }
 
-export const Users: FC<UsersProps> = () => {
-    const { data: users, isLoading, isFetching } = useUsers()
+export const Users: FC<UsersProps> = ({ users }) => {
+    // const { data: users, isLoading, isFetching } = useUsers()
     const { dropDown } = useDropDownStore()
 
-    if (isLoading) return <Loading />
+    // if (isLoading) return <Loading />
 
     return (
         <>
-            {!isFetching ? (
-                <motion.div
-                    className="box"
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{
-                        duration: 0.8,
-                        delay: 0.5,
-                        ease: [0, 0.71, 0.2, 1.01]
-                    }}
-                >
-                    <Table className={`border mt-10 ${dropDown ? "blur-[1.5px]" : null}`}>
-                        <TableCaption>List of all users ({users?.length}).</TableCaption>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>Name</TableHead>
-                                <TableHead>Username</TableHead>
-                                <TableHead>Email</TableHead>
-                                <TableHead>CreatedAt</TableHead>
-                                <TableHead>Attempts</TableHead>
-                                <TableHead>Best Score</TableHead>
-                                <TableHead>Admin</TableHead>
+
+            <motion.div
+                className="box"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.8,
+                    delay: 0.5,
+                    ease: [0, 0.71, 0.2, 1.01]
+                }}
+            >
+                <Table className={`border mt-10 ${dropDown ? "blur-[1.5px]" : null}`}>
+                    <TableCaption>List of all users ({users?.length}).</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Name</TableHead>
+                            <TableHead>Username</TableHead>
+                            <TableHead>Email</TableHead>
+                            <TableHead>CreatedAt</TableHead>
+                            <TableHead>Attempts</TableHead>
+                            <TableHead>Best Score</TableHead>
+                            <TableHead>Admin</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {users?.map((user: UserType) => (
+                            <TableRow key={user.email}>
+                                <TableCell >{user.name}</TableCell>
+                                <TableCell>{user.username}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>{formatDate(user.createdAt)}</TableCell>
+                                <TableCell>{user?.game.length}</TableCell>
+                                <TableCell>
+                                    {user?.game.reduce(
+                                        (maxScore: number, game: GameType) => (game.score > maxScore ? game.score : maxScore),
+                                        0
+                                    )}
+                                </TableCell>
+                                <TableCell>{user.isAdmin ? "✅" : "❌"}</TableCell>
                             </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {users?.map((user: UserType) => (
-                                <TableRow key={user.email}>
-                                    <TableCell >{user.name}</TableCell>
-                                    <TableCell>{user.username}</TableCell>
-                                    <TableCell>{user.email}</TableCell>
-                                    <TableCell>{formatDate(user.createdAt)}</TableCell>
-                                    <TableCell>{user?.game.length}</TableCell>
-                                    <TableCell>
-                                        {user?.game.reduce(
-                                            (maxScore: number, game: GameType) => (game.score > maxScore ? game.score : maxScore),
-                                            0
-                                        )}
-                                    </TableCell>
-                                    <TableCell>{user.isAdmin ? "✅" : "❌"}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </motion.div>
-            ) : (
-                <Loading />
-            )}
+                        ))}
+                    </TableBody>
+                </Table>
+            </motion.div>
         </>
     )
 }
